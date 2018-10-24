c44357afd3868d5c931770057a6abf0b0303e11d
Caleb Gregory <calebgregory@gmail.com>
2018-10-24 16:21:17 -0500

    Installing git in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,5 @@
-ln -sv ~/.dotfiles/.{aliases,exports,functions,path,zshrc,gvimrc.after,vimrc.after,tmux.conf} ~
+sudo dnf install git
+
+echo "installed git"
+which git
+# ln -sv ~/.dotfiles/.{aliases,exports,functions,path,zshrc,gvimrc.after,vimrc.after,tmux.conf} ~
