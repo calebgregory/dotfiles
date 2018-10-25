d784fd17416cc9cf489ed72b02e4cb06c26a9645
Caleb Gregory <calebgregory@gmail.com>
2018-10-24 19:29:38 -0500

    Reorganization, adds awscli, terraform, tmux, zsh, nodejs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,58 +1,71 @@
+#!/usr/bin bash
+
 sudo -i
 
 OS=linux
 ARCH=amd64
 
-# install git
+### primary dependencies
+
 dnf install -y git
 
-echo "installed git"
-which git
-
-# applications
+### applications
 
 # [install Google Chrome](https://www.if-not-true-then-false.com/2010/install-google-chrome-with-yum-on-fedora-red-hat-rhel/)
 dnf install -y fedora-workstation-repositories
 dnf config-manager --set-enabled google-chrome
 dnf install -y google-chrome-stable
 
-# software development
+### languages
 
 # [install Golang](https://golang.org/dl/)
 GO_VERSION="1.11.1"
 GO_ARCHIVE="go$GO_VERSION.$OS-$ARCH.tar.gz"
 
-wget https://dl.google.com/go/$GO_ARCHIVE
-tar -C /usr/local -xzf $GO_ARCHIVE
+wget "https://dl.google.com/go/$GO_ARCHIVE"
+tar -C /usr/local -xzf "$GO_ARCHIVE"
+
+# [install nodejs using nvm](https://github.com/creationix/nvm#install-script)
+#   must
+#     nvm install <version>
+#     nvm use <version>
+#   before node or npm are available
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+### infrastructural tools
 
 # [install Docker](https://docs.docker.com/install/linux/docker-ce/fedora/#install-docker-ce)
 dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 dnf install -y docker-ce
 
+# [install awscli](https://docs.aws.amazon.com/cli/latest/userguide/awscli-install-bundle.html)
+dnf install -y python
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+# [install terraform](https://www.terraform.io/downloads.html)
+TERRAFORM_VERSION="0.11.10"
+TERRAFORM_ARCHIVE="$TERRAFORM_VERSION/terraform_${TERRAFORM_VERSION}_${OS}_${ARCH}.zip"
+
+wget "https://releases.hashicorp.com/terraform/$TERRAFORM_ARCHIVE"
+unzip "$TERRAFORM_ARCHIVE"
+mv ./terraform /usr/local/bin
+
+### code editor, shell
+
 # [install janus distrobution of vim](https://github.com/carlhuda/janus)
-dnf install -y ack ctags ruby gvim
+dnf install -y ack
+dnf install -y ctags
+dnf install -y gvim
+dnf install -y ruby
 gem install rake
 curl -L https://bit.ly/janus-bootstrap | bash
 
-# # install puppet
-#   # add to registry https://puppet.com/docs/puppet/6.0/puppet_platform.html
-# sudo rpm -Uvh https://yum.puppet.com/puppet6/puppet6-release-fedora-27.noarch.rpm
-#   # install puppet-agent https://puppet.com/docs/puppet/5.4/install_linux.html
-# sudo dnf install -y puppet-agent
-#
-# # PATH="/opt/puppetlabs/bin:$PATH"
-#
-# echo "installed puppet"
-#
-# ls -la /opt/puppetlabs/bin
-#
-# echo "starting puppet service"
-#
-# sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
-#
-# git clone https://github.com/calebgregory/manifests /etc/puppetlabs/code/environments/production
-#
-# sudo /opt/puppetlabs/bin/puppet puppet apply
+# install tmux
+dnf install -y tmux
 
-# ln -sv ~/.dotfiles/.{aliases,exports,functions,gitconfig,path,zshrc,gvimrc.after,vimrc.after,tmux.conf} ~
+# install [zsh with oh-my-zsh framework](https://github.com/robbyrussell/oh-my-zsh)
+dnf install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
