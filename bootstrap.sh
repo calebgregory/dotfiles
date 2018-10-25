80bdb528a6fd0c8727ec10b3737798344086171d
Caleb Gregory <calebgregory@gmail.com>
2018-10-24 20:25:30 -0500

    Adds virtualbox and vagrant; installs dotfiles from github

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/usr/bin bash
 
-sudo -i
-
 OS=linux
 ARCH=amd64
+
+sudo -i
+
+mkdir ~/tmp/
+cd ~/tmp/
 
 ### primary dependencies
 
@@ -30,7 +33,8 @@ tar -C /usr/local -xzf "$GO_ARCHIVE"
 #     nvm install <version>
 #     nvm use <version>
 #   before node or npm are available
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+NVM_VERSION="0.33.11"
+curl -o- "https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh" | bash
 
 ### infrastructural tools
 
@@ -53,9 +57,41 @@ wget "https://releases.hashicorp.com/terraform/$TERRAFORM_ARCHIVE"
 unzip "$TERRAFORM_ARCHIVE"
 mv ./terraform /usr/local/bin
 
+# [install virtualbox](https://medium.com/@Joachim8675309/vagrant-and-friends-on-fedora-28-37b8cbc47e47)
+
+  # download virtualbox.repo file for yum
+wget -P /etc/yum.repos.d/ https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
+dnf update -y
+  # install kernel development packages
+dnf install -y \
+  binutils \
+  gcc \
+  make \
+  patch \
+  libgomp \
+  glibc-headers \
+  glibc-devel \
+  kernel-headers \
+  kernel-devel \
+  dkms
+  # install/setup VirtualBox 5.2.x
+dnf install -y VirtualBox-5.2
+/usr/lib/virtualbox/vboxdrv.sh setup
+  # test version
+vboxmanage --version
+  # enable current user
+usermod -a -G vboxusers ${USER}
+
+# [install vagrant](https://medium.com/@Joachim8675309/vagrant-and-friends-on-fedora-28-37b8cbc47e47)
+VAGRANT_VERSION="2.2.0"
+VAGRANT_ARCHIVE="vagrant_${VAGRANT_VERSION}_${OS}_${ARCH}.zip"
+wget "https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION/$VAGRANT_ARCHIVE"
+unzip "$VAGRANT_ARCHIVE"
+mv ./vagrant /usr/local/bin
+
 ### code editor, shell
 
-# [install janus distrobution of vim](https://github.com/carlhuda/janus)
+# [install janus distribution of vim](https://github.com/carlhuda/janus)
 dnf install -y ack
 dnf install -y ctags
 dnf install -y gvim
@@ -69,3 +105,14 @@ dnf install -y tmux
 # install [zsh with oh-my-zsh framework](https://github.com/robbyrussell/oh-my-zsh)
 dnf install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# [install the_silver_searcher](https://github.com/ggreer/the_silver_searcher)
+dnf install -y the_silver_searcher
+
+### download dotfiles
+
+git clone https://github.com/calebgregory/dotfiles ~/.dotfiles
+
+### symlink dotfiles (whitelist-style, to prevent accidents)
+
+ln -s ~/.dotfiles/.{aliases,exports,functions,gitconfig,gvimrc.after,path,tmux.conf,vimrc.after,zshrc} ~
