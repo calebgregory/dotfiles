de01e7589c18d43f525f71a975701fd33c5fc18d
Caleb Gregory <calebgregory@gmail.com>
2025-03-07 17:17:06 -0500

    adds hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,39 @@
+-- watches config changes and reloads automatically
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+-- "handrolled" caffeine from the hammerspoon Getting Started doc
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("☕️")
+    else
+        caffeine:setTitle("😴")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function ()
+  state = hs.caffeinate.toggle("displayIdle")
+
+  if state then
+    hs.alert.show("Caffeine: ON")
+  else
+    hs.alert.show("Caffeine: OFF")
+  end
+
+  setCaffeineDisplay(state)
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, ";", function()
+    hs.application.launchOrFocus("WezTerm.app")
+end)
+
