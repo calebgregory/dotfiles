2e551903937a6fce6bbbcd2fb6a5238d9b93b87a
Caleb Gregory <calebgregory@gmail.com>
2026-01-22 09:50:51 -0500

    adds claude-related utilities

---
+++ .claude/bootstrap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.claude/settings.json ~/.claude/settings.json
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,3 +7,6 @@ dofile('./caffeine.lua')
 dofile('./f-key-mode-toggle.lua')
 dofile('./kid-lock.lua')
 dofile('./pomodoro.lua')
+
+claudeModel = dofile('./claude-model-display.lua')
+claudeModel.start()
